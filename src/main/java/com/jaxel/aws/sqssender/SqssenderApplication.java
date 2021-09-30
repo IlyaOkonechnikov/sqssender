72af9c71321513eea142290805cba89617921938
Ilya Okonechnikov <ok.ilya95kazan@mail.ru>
2021-09-30 14:40:17 +0200

    initial commit

---
+++ src/main/java/com/jaxel/aws/sqssender/SqssenderApplication.java
@@ -0,0 +1,13 @@
+package com.jaxel.aws.sqssender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqssenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqssenderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jaxel/aws/sqssender/SqssenderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaxel.aws.sqssender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqssenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
